bff4766074feff479db1e606f9d7cf365c8ea488
Misha <darkrabbit240@gmail.com>
2026-02-24 14:42:47 +0200

    feat: change theme to grey

--- frontend/src/app/(drawer)/_layout.tsx
+++ frontend/src/app/(drawer)/_layout.tsx
@@ -1,9 +1,35 @@
+import { StyleSheet } from 'react-native';
 import { Drawer } from 'expo-router/drawer';
 
 export default function Layout() {
   return (
-    <Drawer>
-      <Drawer.Screen name={'index'} options={{ title: 'overview', drawerLabel: 'Home' }} />
+    <Drawer
+      screenOptions={{
+        drawerStyle: styles.container
+      }}
+    >
+      <Drawer.Screen
+        name={'index'}
+        options={{
+          title: 'overview',
+          drawerLabel: 'Home',
+          headerStyle: styles.header,
+          headerTitleStyle: styles.headerTitle,
+        }}
+      />
     </Drawer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 250,
+    backgroundColor: '#222025'
+  },
+  header: {
+    backgroundColor: '#413d46'
+  },
+  headerTitle: {
+    color: '#fff',
+  },
+})
--- frontend/src/app/(drawer)/index.tsx
+++ frontend/src/app/(drawer)/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Home screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text} >Home screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#2f2e33',
+  },
+  text: {
+    color: '#fff',
+  }
+})
