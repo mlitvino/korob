479f0624e3941516122f0c2e9b57f856b5fa9c49
Misha <darkrabbit240@gmail.com>
2026-05-17 14:28:34 +0300

    refac: use react-native mockAsyncStorage instead custom implementation

--- frontend/__mocks__/@react-native-async-storage/async-storage.ts
+++ frontend/__mocks__/@react-native-async-storage/async-storage.ts
@@ -1,27 +1,7 @@
-type Storage = {
-  getItem: (key: string) => Promise<string | null>;
-  setItem: (key: string, value: string) => Promise<void>;
-  removeItem: (key: string) => Promise<void>;
-  clear: () => Promise<void>;
-};
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
-const store = new Map<string, string>();
-
-const AsyncStorage: Storage = {
-  getItem: async (key) => store.get(key) ?? null,
-  setItem: async (key, value) => {
-    store.set(key, value);
-  },
-  removeItem: async (key) => {
-    store.delete(key);
-  },
-  clear: async () => {
-    store.clear();
-  },
-};
-
-export default AsyncStorage;
-export const getItem = AsyncStorage.getItem;
-export const setItem = AsyncStorage.setItem;
-export const removeItem = AsyncStorage.removeItem;
-export const clear = AsyncStorage.clear;
+export default mockAsyncStorage;
+export const getItem = mockAsyncStorage.getItem;
+export const setItem = mockAsyncStorage.setItem;
+export const removeItem = mockAsyncStorage.removeItem;
+export const clear = mockAsyncStorage.clear;
